c42c829515e2dd6cf157a59e5f71d87dcc604283
Shaheer <sherry.aitools1@gmail.com>
2025-12-19 10:43:42 +0500

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-cd /data/data/com.termux/files/home/email-api
-
-cat > api.py << 'EOF'
-from app.main import app
-
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 8000))
-    app.run(host='0.0.0.0', port=port)
